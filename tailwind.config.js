580d0e9b4ed14e08eb8a498673285317e10378b7
khaled J Muhammad <khaleedkblings@gmail.com>
2025-11-29 20:29:56 +0100

    Add Tailwind CSS configuration with custom theme
    Configure Tailwind CSS with custom colors.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tertiary: "#bdb76b",
+        "text-foreground": "#504230",
+        "muted-foreground": "#8a775c",
+        border: "#ddddb5",
+
+        background: "#f5f5db",
+        secondary: "#ededd4",
+        primary: "#a68b64",
+      },
+    },
+  },
+  plugins: [],
+};
