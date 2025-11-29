85f6ae7b7ddd452d5e310b2116b07cbf9c0e60c7
khaled J Muhammad <khaleedkblings@gmail.com>
2025-11-29 19:54:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
   },
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Literata", "serif"],
-        headline: ["Literata", "serif"],
-        code: ["monospace"],
-      },
-      colors: {
-        secondary: "#ededd4",
-        primary: "#a68b64",
-      },
-    },
+  build: {
+    outDir: "dist", // ensures the production build goes to dist
   },
-  content: [
-    "index.html",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
 });
