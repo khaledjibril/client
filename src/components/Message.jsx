0773daa1d1784ae3d7ffddfd50bd71f01cc4185a
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-12-23 13:18:15 +0100

    Updates: Changed the message title and content to white and added transition

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,12 +1,18 @@
 "use strict";
 
-const Message = ({ title, message, backgroundColor }) => {
+const Message = ({ title, message, backgroundColor, isVisible }) => {
   return (
     <div
-      className={`flex flex-col gap-3 sm:gap-6 rounded-lg w-md sm:w-xl absolute bottom-10 right-6 px-12 py-10 z-50 ${backgroundColor}`}
+      className={`flex flex-col gap-3 sm:gap-6 rounded-lg w-md sm:w-xl absolute bottom-10 right-6 px-12 py-10 z-50 ${backgroundColor} ${
+        isVisible
+          ? "opacity-100 translate-y-0 duration-500 ease-in-out"
+          : "opacity-0 translate-y-6 pointer-events-none duration-300"
+      }`}
     >
-      <h1 className="font-medium capitalize leading-7 sm:leading-0">{title}</h1>
-      <p className="leading-7 text-[1.4rem]">{message}</p>
+      <h1 className="font-medium capitalize leading-7 sm:leading-0 text-white">
+        {title}
+      </h1>
+      <p className="leading-7 text-[1.4rem] text-white">{message}</p>
     </div>
   );
 };
